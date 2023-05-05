23be5daa79182f2131b43914933465c3ef2c229b
while-TuRe <993968861@qq.com>
2023-05-05 18:57:37 +0800

    add a gpu callback

--- train.py
+++ train.py
@@ -7,6 +7,5 @@ from comer.lit_comer import LitCoMER
 cli = LightningCLI(
     LitCoMER,
     CROHMEDatamodule,
-    save_config_overwrite=True,
-    trainer_defaults={"plugins": DDPPlugin(find_unused_parameters=False)},
+    save_config_overwrite=True
 )
